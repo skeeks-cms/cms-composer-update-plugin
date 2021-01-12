29be4b53dfa848df91936cbe0b74fcd8c9fdf293
Semenov <semenov@skeeks.com>
2021-01-12 12:01:21 +0300

    dev

--- src/Plugin.php
+++ src/Plugin.php
@@ -66,8 +66,14 @@ class Plugin implements PluginInterface, EventSubscriberInterface
             fwrite($fp, time());
             fclose($fp);
         }
+    }
+    
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
 
-
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
     }
 
     /**
